58546c31ed77789c2c132028b831ff754b279804
Isaac Telloian <iketelloian@fresnostate.edu>
2022-05-06 10:02:50 -0700

    finished laymen's terms

--- test_2.py
+++ test_2.py
@@ -4,8 +4,13 @@
 The program is trying to use the Final.txt to display numbers of grades,
 the average grade, and the percentage of grades that are above the average.
 Display Number of test_scores in list (Final.txt)
-Add numbers in list and divide by number of test_scores in list
-
+Add numbers in list and divide by number of test_scores in list to get
+average_grade
+Compare test_scores on Final.txt to average_grade;
+if test_score > average_score
+add 1 to num_scores_above_average
+use num_scores_above_average / number of test scores = percent_above_average
+return num_grades, avg_grade, and percent_above_average
 (For Test;)
 Number of grades: 24
 Average grade: 83.25
