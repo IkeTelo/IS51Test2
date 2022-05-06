e681c91c749526cffc941f240c0a8d60b1e6864f
Isaac Telloian <iketelloian@fresnostate.edu>
2022-05-06 09:56:25 -0700

    first commit

---
+++ test_2.py
@@ -0,0 +1,14 @@
+# Laymen's Terms
+"""
+
+The program is trying to use the Final.txt to display numbers of grades,
+the average grade, and the percentage of grades that are above the average.
+Display Number of test_scores in list (Final.txt)
+Add numbers in list and divide by number of test_scores in list
+
+(For Test;)
+Number of grades: 24
+Average grade: 83.25
+% of grades above average: 54.17
+"""
+
